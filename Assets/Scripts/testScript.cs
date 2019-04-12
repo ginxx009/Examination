f471ed00fa7a96d0119a15011a6a922e020f21e7
Ginx009 <macandili09@gmail.com>
2019-04-12 18:35:50 +0800

    tesst

---
+++ Assets/Scripts/testScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+public class testScript : MonoBehaviour
+{
+   
+    private void Start()
+    {
+        TextAsset myTextAsset = Resources.Load("dictionary") as TextAsset;
+        string myString = myTextAsset.text;
+
+        Debug.Log(myString);
+    }
+}
