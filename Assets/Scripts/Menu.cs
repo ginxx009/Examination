16d1e2b3822e1c22dcb58b92fbeee65c71052da0
Ginx009 <macandili09@gmail.com>
2019-04-12 13:09:31 +0800

    Menu Functionalities

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,21 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
+    public GameObject[] menus;
+    public GameObject credits;
+
     public void Play()
     {
-        
+        SceneManager.LoadScene("Word Scramble", LoadSceneMode.Single);
     }
 
     public void Credits()
     {
+        foreach(GameObject menu in menus)
+        {
+            menu.SetActive(false);
+        }
+        credits.SetActive(true);
+    }
 
+    public void Back()
+    {
+        foreach (GameObject menu in menus)
+        {
+            menu.SetActive(true);
+        }
+        credits.SetActive(false);
     }
 
     public void ExitGame()
     {
-        
+        Application.Quit();
     }
 }
