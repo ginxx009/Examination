08e90b5b7f89ce93904942ff0329237901ac56a2
Ginx009 <macandili09@gmail.com>
2019-04-12 12:46:51 +0800

    Add Menu Credit and Exit

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public void Play()
+    {
+        
+    }
+
+    public void Credits()
+    {
+
+    }
+
+    public void ExitGame()
+    {
+        
+    }
+}
