b168c8a7471099ca79161a8d6ce6fb5d6ba1fdec
Ginx009 <macandili09@gmail.com>
2019-04-13 14:24:47 +0800

    test endless

--- Assets/Scripts/testScript.cs
+++ Assets/Scripts/testScript.cs
@@ -2,14 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.UI;
 public class testScript : MonoBehaviour
 {
-   
+    public static List<string> textArray;
+
+    public Text textComp;
+
+    public int[] rowsToReadFrom = new int[1] {0};
+
+    public string FileName;
+
+    private TextAsset myTextAsset;
+
+    private int index = 0;
+
+
     private void Start()
     {
-        TextAsset myTextAsset = Resources.Load("dictionary") as TextAsset;
+        myTextAsset = Resources.Load("dictionary") as TextAsset;
         string myString = myTextAsset.text;
+    }
 
-        Debug.Log(myString);
+    public void readTextFile()
+    {
+        textComp.text = "";
+        rowsToReadFrom[0] = index;
+        textArray = myTextAsset.text.Split('\n').ToList();
+        textComp.text += textArray[rowsToReadFrom[0]] + "\n";
+
+        index += 1;
+
     }
 }
